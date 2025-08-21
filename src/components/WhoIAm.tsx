facbaf56c22f5ebc0bac9d374bc15f2be74ce34c
melnyresults <ivanmelnychenkoo@gmail.com>
2025-08-21 15:56:44 -0400

    Add background image to WhoIAm section

--- src/components/WhoIAm.tsx
+++ src/components/WhoIAm.tsx
@@ -16,13 +16,15 @@ const WhoIAm: React.FC<WhoIAmProps> = ({ currentLanguage }) => {
   const t = translations[currentLanguage];
 
   return (
-    <section id="about" ref={ref} className="py-20 bg-white relative overflow-hidden">
-      {/* Passport Pattern Background */}
-      <div className="absolute inset-0 opacity-5">
-        <div className="w-full h-full" style={{
-          backgroundImage: `url("data:image/svg+xml,%3Csvg width='60' height='60' viewBox='0 0 60 60' xmlns='http://www.w3.org/2000/svg'%3E%3Cg fill='none' fill-rule='evenodd'%3E%3Cg fill='%23002c5f' fill-opacity='0.1'%3E%3Cpath d='M30 30c0-16.569 13.431-30 30-30v60c-16.569 0-30-13.431-30-30zM0 30c0 16.569 13.431 30 30 30V0C13.431 0 0 13.431 0 30z'/%3E%3C/g%3E%3C/g%3E%3C/svg%3E")`,
-          backgroundSize: '60px 60px'
-        }}></div>
+    <section id="about" ref={ref} className="py-20 relative overflow-hidden">
+      {/* Background Image */}
+      <div className="absolute inset-0">
+        <img
+          src="https://i.ibb.co/xScWhCGT/shutterstock-2486520637-1.png"
+          alt="Professional background"
+          className="w-full h-full object-cover"
+        />
+        <div className="absolute inset-0 bg-white/85"></div>
       </div>
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 relative">
