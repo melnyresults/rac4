988bfc85f53aa9379cc50d75b721f42abad2fe13
melnyresults <ivanmelnychenkoo@gmail.com>
2025-08-26 21:45:21 -0400

    Updated LoadingAnimation.tsx

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -27,7 +27,7 @@ const LoadingAnimation: React.FC = () => {
             <img
               src="https://i.ibb.co/Csb2gT8X/Untitled-design-3.png"
               alt="RAC Immigration Logo"
-              className="w-full h-full object-contain scale-300"
+              className="w-full h-full object-contain scale-110"
             />
           </motion.div>
         </div>
