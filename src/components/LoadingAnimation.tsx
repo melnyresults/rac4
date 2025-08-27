65a1c9722a653b5ce84e09e9bb95eac75b230506
melnyresults <ivanmelnychenkoo@gmail.com>
2025-08-26 21:42:24 -0400

    Fix missing JSX closing tag in LoadingAnimation

--- src/components/LoadingAnimation.tsx
+++ src/components/LoadingAnimation.tsx
@@ -40,6 +40,7 @@ const LoadingAnimation: React.FC = () => {
           R506278
         </motion.div>
       </motion.div>
+      </div>
       
       <motion.div
         initial={{ opacity: 0, y: 20 }}
